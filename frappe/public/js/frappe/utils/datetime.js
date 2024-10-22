f75d6b3656f4076acc03eda4950cb49a47cd3600
ahmadsmdn <a.samadani110@gmail.com>
2024-10-22 11:12:05 -0700

    changing in date formats and adding jalali suport to add_month func

--- frappe/public/js/frappe/utils/datetime.js
+++ frappe/public/js/frappe/utils/datetime.js
@@ -95,7 +95,10 @@ $.extend(frappe.datetime, {
 	},
 
 	add_months: function (d, months) {
-		return moment(d).add(months, "months").format();
+		if(frappe.boot.user.defaults.calendar_type === 'jalali'){monthformat='jmonths'}
+		else{monthformat='months'}
+		return moment(d).add(months, monthformat).format();
+		
 	},
 
 	week_start: function () {
@@ -188,7 +191,9 @@ $.extend(frappe.datetime, {
 					.replace(/(\W|^)DD(\W|$)/, '$1jDD$2')
 					.replace(/(\W|^)D(\W|$)/, '$1jD$2')
 					.replace(/(\W|^)M(\W|$)/, '$1jM$2')
-					.replace(/(\W|^)YY(\W|$)/, '$1jYY$2');
+					.replace(/(\W|^)YY(\W|$)/, '$1jYY$2')
+					.replace(/(\W|^)Do(\W|$)/, '$1jDo$2')
+					.replace(/(\W|^)MMMM(\W|$)/, '$1jMMMM$2');
 			default:
 				return format;
 		}
@@ -221,9 +226,9 @@ $.extend(frappe.datetime, {
 	global_date_format: function (d) {
 		var m = moment(d);
 		if (m._f && m._f.indexOf("HH") !== -1) {
-			return m.format("Do MMMM YYYY, hh:mm A");
+			return m.format(this.fixDateFormatForCalendar("Do MMMM YYYY, hh:mm A"));
 		} else {
-			return m.format("Do MMMM YYYY");
+			return m.format(this.fixDateFormatForCalendar("Do MMMM YYYY"));
 		}
 	},
 
